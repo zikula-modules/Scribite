7b191e85bd65ce6064b028bdc4a03e69228b19c9
Sven Schomacker <hilope@googlemail.com>
2008-06-03 16:54:43 +0000

    some additions for 2.1 and new lang nds

---
+++ scribite/v2/modules/scribite/pnlang/nds_utf8/global.php
@@ -0,0 +1,97 @@
+<?php
+// ----------------------------------------------------------------------
+// PostNuke Content Management System
+// Copyright (C) 2002 by the PostNuke Development Team.
+// http://www.postnuke.com/
+// ----------------------------------------------------------------------
+// Based on:
+// PHP-NUKE Web Portal System - http://phpnuke.org/
+// Thatware - http://thatware.org/
+// ----------------------------------------------------------------------
+// LICENSE
+//
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License (GPL)
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+//
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+//
+// To read the license please visit http://www.gnu.org/copyleft/gpl.html
+// ----------------------------------------------------------------------
+// Author of file: sven schomacker
+// Purpose of file: Translation files
+// version 2.1
+// ----------------------------------------------------------------------
+ 
+// common
+define('_EDITOR', 'scribite!');
+define('_EDITORINFO', 'WYSIWYG vör Postnuke CMS');
+define('_EDITORNOCONFCHANGE', 'Konfiguration nich opfrischt');
+define('_EDITORMODTITLE', 'Module');
+define('_EDITORUPDATE', 'Instellen sekern');
+define('_EDITORUPDATED', 'Instellen opfrischt');
+define('_EDITORSPATH', 'Editorpad');
+define('_JSWARNING', 'De Orner /modules/scribite/pnincludes is nich nödig un kann löscht weern!');
+define('_ADDMOD', 'Modul tovögen');
+define('_EDITMOD', 'Modul bewerken');
+define('_DELMOD', 'Modul wegdoon');
+define('_DELMODQUESTION', 'Schull dat Modul echt wegdoon weern?');
+define('_MODNAME', 'Modulnaam');
+define('_MODFUNCS', 'Modulfunkschoon (kommascheden, "all" för all Funkschoonen)');
+define('_MODAREAS', 'Textarea-ID\'s (kommascheden, "all" för all Textareas)');
+define('_MODEDITOR', 'Editor');
+define('_WARNING', 'Wohrschau'); 
+define('_JSQACTIVATED', 'JS Quick Tags sünt aktiv.<br />För een süver Funkschoon schull dat deaktiviert weern in de Postnuke Instellen ');
+define('_DEFAULTEDITOR', 'Standardeditor');
+
+// xinha
+define('_XINHASETTINGS', 'Xinha');
+define('_XINHALANGUAGE', 'Editorspraak');
+define('_XINHASKIN', 'Utsehn');
+define('_XINHABARMODE', 'Symbolliest');
+define('_XINHABARMODE0', 'vull');
+define('_XINHABARMODE1', 'reduziert');
+define('_XINHABARMODE2', 'mini');
+define('_XINHAWINDOW', 'Editorbredde un -hööcht');
+define('_XINHASTATUSBAR', 'Statusbar');
+define('_XINHASTYLE', 'Editor-Stylesheet');
+define('_XINHACONVERTURLS', 'Urls in Links ümmodeln');
+define('_XINHASHOWLOADING', 'Oploden von de Editor wiesen');
+define('_XINHAPLUGINS', 'Xinha Plugins (<a href="http://xinha.python-hosting.com/wiki/Plugins" target="_blank">documentation</a>)');
+
+// tinymce
+define('_TINYMCESETTINGS', 'TinyMCE');
+define('_TINYMCELANGUAGE', 'Editorspraak');
+define('_TINYMCETHEME', 'Utsehn');
+define('_TINYMCEDATEFORMAT', 'Datumsformat');
+define('_TINYMCETIMEFORMAT', 'Tietformat');
+define('_TINYMCEWINDOW', 'Editorbredde un -hööcht');
+define('_TINYMCESTYLE', 'Editor-Stylesheet');
+define('_TINYMCEASK', 'Editor erst op Nafraag wiesen');
+define('_TINYMCEPLUGINS', 'TinyMCE Plugins (<a href="http://wiki.moxiecode.com/index.php/TinyMCE:Plugins" target="_blank">documentation</a>)');
+define('_TINYMCEMCPUK', 'TinyMCEMCPUK');
+
+// fckeditor
+define('_FCKEDITORSETTINGS', 'FCKeditor');
+define('_FCKEDITORLANGUAGE', 'Editorspraak');
+define('_FCKEDITORAUTOLANG', 'Spraak automatisch instellen');
+define('_FCKEDITORBARMODE', 'Symbolliest');
+define('_FCKEDITORSKIN', 'Utsehn');
+define('_FCKEDITORWINDOW', 'Editorbredde un -hööcht');
+define('_FCKEDITORPLUGINS', 'FCKeditor Plugins (<a href="http://wiki.fckeditor.net/" target="_blank">documentation</a>)');
+
+// openwysiwyg
+define('_OPENWYSIWYGSETTINGS', 'openWYSIWYG');
+define('_OPENWYSIWYGBARMODE', 'Symbolliest');
+define('_OPENWYSIWYGBARMODE0', 'vull');
+define('_OPENWYSIWYGBARMODE1', 'reduziert');
+define('_OPENWYSIWYGWINDOW', 'Editorbredde un -hööcht');
+
+// NicEdit
+define('_NICEDITORSETTINGS', 'NicEdit');
+define('_NICEDITORFULLPANEL', 'vull Symbolliest');
+
