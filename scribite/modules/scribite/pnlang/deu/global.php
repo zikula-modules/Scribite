103f1ed42790d2ac6d7a777d3c996a5628c10d3b
Sven Schomacker <hilope@googlemail.com>
2008-08-25 07:44:41 +0000

    charset corrections and preparations for next release

--- scribite/modules/scribite/pnlang/deu/global.php
+++ scribite/modules/scribite/pnlang/deu/global.php
@@ -15,23 +15,23 @@
 
 // common
 define('_EDITOR', 'scribite!');
-define('_EDITORINFO', 'WYSIWYG fï¿½r Zikula');
+define('_EDITORINFO', 'WYSIWYG für Zikula');
 define('_EDITORNOCONFCHANGE', 'Konfiguration nicht aktualisiert');
 define('_EDITORMODTITLE', 'Module');
 define('_EDITORUPDATE', 'Konfiguration speichern');
 define('_EDITORUPDATED', 'Konfiguration aktualisiert');
 define('_EDITORSPATH', 'Editorpfad');
-define('_JSWARNING', 'Der Ordner /modules/scribite/pnincludes ist nicht mehr nï¿½tig und kann gelï¿½scht werden!');
-define('_ADDMOD', 'Modul hinzufï¿½gen');
+define('_JSWARNING', 'Der Ordner /modules/scribite/pnincludes ist nicht mehr nötig und kann gelöscht werden!');
+define('_ADDMOD', 'Modul hinzufügen');
 define('_EDITMOD', 'Modul bearbeiten');
-define('_DELMOD', 'Modul lï¿½schen');
-define('_DELMODQUESTION', 'Soll das Modul wirklich gelï¿½scht werden?');
+define('_DELMOD', 'Modul löschen');
+define('_DELMODQUESTION', 'Soll das Modul wirklich gelöscht werden?');
 define('_MODNAME', 'Modulname');
-define('_MODFUNCS', 'Modulfunktionen (kommasepariert, "all" fï¿½r alle Funktionen)');
-define('_MODAREAS', 'Textarea-ID\'s (kommasepariert, "all" fï¿½r alle Textareas)');
+define('_MODFUNCS', 'Modulfunktionen (kommasepariert, "all" für alle Funktionen)');
+define('_MODAREAS', 'Textarea-ID\'s (kommasepariert, "all" für alle Textareas)');
 define('_MODEDITOR', 'Editor');
 define('_WARNING', 'Warnung');
-define('_JSQACTIVATED', 'JS Quick Tags sind aktiviert.<br />Fï¿½r eine einwandfreie Funktion sollte dies in den Zikula Einstellungen deaktiviert werden');
+define('_JSQACTIVATED', 'JS Quick Tags sind aktiviert.<br />Für eine einwandfreie Funktion sollte dies in den Zikula Einstellungen deaktiviertï¿½ werden');
 define('_DEFAULTEDITOR', 'Standardeditor');
 
 // xinha
@@ -42,7 +42,7 @@ define('_XINHABARMODE', 'Symbolleiste');
 define('_XINHABARMODE0', 'voll');
 define('_XINHABARMODE1', 'reduziert');
 define('_XINHABARMODE2', 'mini');
-define('_XINHAWINDOW', 'Editorbreite und -hï¿½he');
+define('_XINHAWINDOW', 'Editorbreite und -höhe');
 define('_XINHASTATUSBAR', 'Statusbar');
 define('_XINHASTYLE', 'Editor-Stylesheet');
 define('_XINHACONVERTURLS', 'Urls in Links umwandeln');
@@ -55,7 +55,7 @@ define('_TINYMCELANGUAGE', 'Editorsprache');
 define('_TINYMCETHEME', 'Theme');
 define('_TINYMCEDATEFORMAT', 'Datumsformat');
 define('_TINYMCETIMEFORMAT', 'Zeitformat');
-define('_TINYMCEWINDOW', 'Editorbreite und -hï¿½he');
+define('_TINYMCEWINDOW', 'Editorbreite und -höhe');
 define('_TINYMCESTYLE', 'Editor-Stylesheet');
 define('_TINYMCEASK', 'Editor erst auf Nachfrage aktivieren');
 define('_TINYMCEPLUGINS', 'TinyMCE Plugins (<a href="http://wiki.moxiecode.com/index.php/TinyMCE:Plugins" target="_blank">documentation</a>)');
@@ -67,7 +67,7 @@ define('_FCKEDITORLANGUAGE', 'Editorsprache');
 define('_FCKEDITORAUTOLANG', 'automatische Sprachwahl');
 define('_FCKEDITORBARMODE', 'Symbolleiste');
 define('_FCKEDITORSKIN', 'Skin');
-define('_FCKEDITORWINDOW', 'Editorbreite und -hï¿½he');
+define('_FCKEDITORWINDOW', 'Editorbreite und -höhe');
 define('_FCKEDITORPLUGINS', 'FCKeditor Plugins (<a href="http://wiki.fckeditor.net/" target="_blank">documentation</a>)');
 
 // openwysiwyg
@@ -75,7 +75,7 @@ define('_OPENWYSIWYGSETTINGS', 'openWYSIWYG');
 define('_OPENWYSIWYGBARMODE', 'Symbolleiste');
 define('_OPENWYSIWYGBARMODE0', 'voll');
 define('_OPENWYSIWYGBARMODE1', 'reduziert');
-define('_OPENWYSIWYGWINDOW', 'Editorbreite und -hï¿½he');
+define('_OPENWYSIWYGWINDOW', 'Editorbreite und -höhe');
 
 // NicEdit
 define('_NICEDITORSETTINGS', 'NicEdit');
