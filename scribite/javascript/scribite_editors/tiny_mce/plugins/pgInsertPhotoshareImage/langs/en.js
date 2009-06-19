97100b60debde0a128fc2b6a378020b65c3cea93
Sven Schomacker <hilope@googlemail.com>
2009-06-19 23:09:42 +0000

    removing old version of TinyMCE

--- scribite/javascript/scribite_editors/tiny_mce/plugins/pgInsertPhotoshareImage/langs/en.js
+++
@@ -1,8 +0,0 @@
-// UK lang variables
-
-/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
-
-tinyMCE.addToLang('',{
-	pgInsertPhotoshareImage_title : 'Insert photoshare image link',
-	pgInsertPhotoshareImage_desc : 'Insert link to a photoshare image'
-});
--- scribite/javascript/scribite_editors/tiny_mce/plugins/pgInsertPublicationLink/langs/en.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('',{
-pgInsertPublicationLink_title : 'Insert pagesetter publication link',
-pgInsertPublicationLink_desc : 'Insert link to a pagesetter publication'
-});
