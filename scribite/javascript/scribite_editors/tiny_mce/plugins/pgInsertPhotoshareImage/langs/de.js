a69fdac31d5872ecdc285fa286e092a8db360ef1
Sven Schomacker <hilope@googlemail.com>
2009-11-18 21:09:20 +0000

    removed editors from /javascript / moved to modules folder

--- scribite/javascript/scribite_editors/tiny_mce/plugins/pgInsertPhotoshareImage/langs/de.js
+++
@@ -1,8 +0,0 @@
-// UK lang variables
-
-/* Remember to namespace the language parameters lang_<your plugin>_<some name> */
-
-tinyMCE.addToLang('',{
-	pgInsertPhotoshareImage_title : 'Photoshare-Link einfügen',
-	pgInsertPhotoshareImage_desc : 'Link auf ein Photosharebild einfügen'
-});
--- scribite/javascript/scribite_editors/tiny_mce/plugins/pgInsertPublicationLink/langs/de.js
+++
@@ -1,6 +0,0 @@
-// UK lang variables
-
-tinyMCE.addToLang('',{
-pgInsertPublicationLink_title : 'Pagesetter-Link einfügen',
-pgInsertPublicationLink_desc : 'Link auf eine Pagesetter-Publikation einfügen'
-});
