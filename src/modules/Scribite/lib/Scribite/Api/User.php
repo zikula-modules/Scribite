25672b443a733c9e7cee4dc0b8c5fef2cca0d1c7
Drak <drak@zikula.org>
2011-02-15 10:06:25 +0545

    Move method to module API.

--- src/modules/Scribite/lib/Scribite/Api/User.php
+++ src/modules/Scribite/lib/Scribite/Api/User.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Zikula Application Framework
  *
@@ -7,9 +8,9 @@
  * @license    GNU/GPL - http://www.gnu.org/copyleft/gpl.html
  * @author     sven schomacker <hilope@gmail.com>
  */
-
 class Scribite_Api_User extends Zikula_Api
 {
+
     // load module config from db into array or list all modules with config
     public function getModuleConfig($args)
     {
@@ -23,7 +24,7 @@ class Scribite_Api_User extends Zikula_Api
         } else {
             $dbtables = DBUtil::getTables();
             $scribitecolumn = $dbtables['scribite_column'];
-            $where = "$scribitecolumn[modname] = '".$args['modulename']."'";
+            $where = "$scribitecolumn[modname] = '" . $args['modulename'] . "'";
             $item = DBUtil::selectObjectArray('scribite', $where);
 
             if ($item == false) {
@@ -49,7 +50,7 @@ class Scribite_Api_User extends Zikula_Api
     {
         $editorname = $args['editorname'];
         $editors = array();
-        $path = rtrim($this->getVar('editors_path'),'/');
+        $path = rtrim($this->getVar('editors_path'), '/');
         $editorsdir = opendir($path);
         while (false !== ($f = readdir($editorsdir))) {
             if ($f != '.' && $f != '..' && $f != 'CVS' && !preg_match('/\./', $f)) {
@@ -66,8 +67,7 @@ class Scribite_Api_User extends Zikula_Api
         // list will give a full list of installed editors
         if ($editorname == 'list') {
             return $editors;
-        }
-        else { // check if given editorname is available
+        } else { // check if given editorname is available
             if (in_array($editorname, $editors)) {
                 $editor_active = 1;
             } else {
@@ -75,7 +75,6 @@ class Scribite_Api_User extends Zikula_Api
             }
             return $editor_active;
         }
-
     }
 
 // load IM/EFM settings for Xinha and pass vars to session
@@ -86,9 +85,9 @@ class Scribite_Api_User extends Zikula_Api
         $editors_path = $this->getVar('editors_path');
         require_once $editors_path . '/xinha/contrib/php-xinha.php';
 
-        $zikulaBaseURI = rtrim(System::getBaseUri(),'/');
-        $zikulaBaseURI = ltrim($zikulaBaseURI,'/');
-        $zikulaRoot = rtrim($_SERVER['DOCUMENT_ROOT'],'/');
+        $zikulaBaseURI = rtrim(System::getBaseUri(), '/');
+        $zikulaBaseURI = ltrim($zikulaBaseURI, '/');
+        $zikulaRoot = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
 
         // define backend configuration for the plugin
         $IMConfig = array();
@@ -106,17 +105,314 @@ class Scribite_Api_User extends Zikula_Api
 
         $IMConfig['max_filesize_kb_link'] = 5000;
         // maximum size for uploading files in 'insert link' mode (5000 kB here)
-
         // Maximum upload folder size in Megabytes.
         // Use 0 to disable limit
         $IMConfig['max_foldersize_mb'] = 0;
 
-        $IMConfig['allowed_image_extensions'] = array("jpg","gif","png");
-        $IMConfig['allowed_link_extensions'] = array("jpg","gif","pdf","ip","txt",
-                "psd","png","html","swf",
-                "xml","xls");
+        $IMConfig['allowed_image_extensions'] = array("jpg", "gif", "png");
+        $IMConfig['allowed_link_extensions'] = array("jpg", "gif", "pdf", "ip", "txt",
+                "psd", "png", "html", "swf",
+                "xml", "xls");
 
         xinha_pass_to_php_backend($IMConfig);
         return $IMConfig;
+    }
+
+    /**
+     * Initialise scribite for requested areas.
+     *
+     * @param array $args Text area: 'area', Module name: 'modulename'.
+     *
+     * @return string
+     */
+    public static function loader($args)
+    {
+        $dom = ZLanguage::getModuleDomain('Scribite');
+        // Argument checks
+        if (!isset($args['areas'])) {
+            return LogUtil::registerError(__('Error! Scribite_Api_User::loader() "area" argument was empty.', $dom));
+        }
+        if (!isset($args['modulename'])) {
+            $args['modulename'] = ModUtil::getName();
+        }
+
+        $module = $args['modulename'];
+
+        // Security check if user has COMMENT permission for scribite and module
+        if (!SecurityUtil::checkPermission('Scribite::', "$module::", ACCESS_COMMENT)) {
+            return;
+        }
+
+        // check for editor argument, if none given the default editor will be used
+        if (!$args['editor']) {
+            // get default editor from config
+            $defaulteditor = ModUtil::getVar('Scribite', 'DefaultEditor');
+            if ($defaulteditor == '-') {
+                return; // return if no default is set and no arg is given
+                // id given editor doesn't exist use default editor
+            } else {
+                $args['editor'] = $defaulteditor;
+            }
+        }
+
+        // check if editor argument exists, load default if not given
+        if (ModUtil::apiFunc('Scribite', 'user', 'getEditors', array('editorname' => $args['editor']))) {
+
+            // set some general parameters
+            $zBaseUrl = rtrim(System::getBaseUrl(), '/');
+            $zikulaThemeBaseURL = "$zBaseUrl/themes/" . DataUtil::formatForOS(UserUtil::getTheme());
+            $zikulaBaseURI = rtrim(System::getBaseUri(), '/');
+            $zikulaBaseURI = ltrim($zikulaBaseURI, '/');
+            $zikulaRoot = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
+
+            // prepare view instance
+            $view = Zikula_View::getInstance('Scribite');
+            $view->setCaching(false);
+            $view->assign(ModUtil::getVar('Scribite'));
+            $view->assign('modname', $args['modulename']);
+            $view->assign('zBaseUrl', $zBaseUrl);
+            $view->assign('zikulaBaseURI', $zikulaBaseURI);
+            $view->assign('zikulaRoot', $zikulaRoot);
+            $view->assign('editor_dir', $args['editor']);
+            $view->assign('zlang', ZLanguage::getLanguageCode());
+
+            // check for modules installed providing plugins and load specific javascripts
+            if (ModUtil::available('photoshare')) {
+                PageUtil::AddVar('javascript', 'modules/photoshare/javascript/findimage.js');
+            }
+            if (ModUtil::available('mediashare')) {
+                PageUtil::AddVar('javascript', 'modules/mediashare/javascript/finditem.js');
+            }
+            if (ModUtil::available('pagesetter')) {
+                PageUtil::AddVar('javascript', 'modules/pagesetter/javascript/findpub.js');
+            }
+            if (ModUtil::available('folder')) {
+                PageUtil::AddVar('javascript', 'modules/folder/javascript/selector.js');
+            }
+            if (ModUtil::available('MediaAttach')) {
+                PageUtil::AddVar('javascript', 'modules/MediaAttach/javascript/finditem.js');
+            }
+            if (ModUtil::available('Files')) {
+                PageUtil::AddVar('javascript', 'modules/Files/javascript/getFiles.js');
+            }
+
+            // main switch for choosen editor
+            switch ($args['editor']) {
+
+                case 'xinha':
+
+                    // get xinha config if editor is active
+                    // get plugins for xinha
+                    $xinha_listplugins = ModUtil::getVar('Scribite', 'xinha_activeplugins');
+                    if ($xinha_listplugins != '') {
+                        $xinha_listplugins = unserialize($xinha_listplugins);
+                        /* if (in_array('ExtendedFileManager', $xinha_listplugins)) {
+                          $view->assign('EFMConfig', true);
+                          } else { */
+                        $view->assign('EFMConfig', false);
+                        //}
+                        $xinha_listplugins = '\'' . DataUtil::formatForDisplay(implode('\', \'', $xinha_listplugins)) . '\'';
+                    }
+
+                    // prepare areas for xinha
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } elseif ($args['areas'][0] == "PagEd") {
+                        $modareas = 'PagEd';
+                    } else {
+                        $modareas = '\'' . DataUtil::formatForDisplay(implode('\', \'', $args['areas'])) . '\'';
+                    }
+
+                    // load Prototype
+                    PageUtil::AddVar('javascript', 'prototype');
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+                    $view->assign('xinha_listplugins', $xinha_listplugins);
+
+                    // end xinha
+                    break;
+
+                case 'tiny_mce':
+                    // get TinyMCE config if editor is active
+                    // get plugins for tiny_mce
+                    $tinymce_listplugins = ModUtil::getVar('Scribite', 'tinymce_activeplugins');
+                    if ($tinymce_listplugins != '') {
+                        $tinymce_listplugins = unserialize($tinymce_listplugins);
+                        $tinymce_listplugins = DataUtil::formatForDisplay(implode(',', $tinymce_listplugins));
+                    }
+                    // prepare areas for tiny_mce
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } elseif ($args['areas'][0] == "PagEd") {
+                        $modareas = 'PagEd';
+                    } else {
+                        $modareas = DataUtil::formatForDisplay(implode(',', $args['areas']));
+                    }
+
+                    // check for allowed html
+                    $AllowableHTML = System::getVar('AllowableHTML');
+                    $disallowedhtml = array();
+                    while (list($key, $access) = each($AllowableHTML)) {
+                        if ($access == 0) {
+                            $disallowedhtml[] = DataUtil::formatForDisplay($key);
+                        }
+                    }
+
+                    // pass disallowed html
+                    $disallowedhtml = implode(',', $disallowedhtml);
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+                    $view->assign('tinymce_listplugins', $tinymce_listplugins);
+                    $view->assign('disallowedhtml', $disallowedhtml);
+
+                    // end tiny_mce
+                    break;
+
+                case 'fckeditor':
+                    // get FCKeditor config if editor is active
+                    // prepare areas for xinha
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } elseif ($args['areas'][0] == "PagEd") {
+                        $modareas = 'PagEd';
+                    } else {
+                        $modareas = $args['areas'];
+                    }
+
+                    // check for allowed html
+                    $AllowableHTML = System::getVar('AllowableHTML');
+                    $disallowedhtml = array();
+                    while (list($key, $access) = each($AllowableHTML)) {
+                        if ($access == 0) {
+                            $disallowedhtml[] = DataUtil::formatForDisplay($key);
+                        }
+                    }
+
+                    // load Prototype
+                    PageUtil::AddVar('javascript', 'javascript/ajax/prototype.js');
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+                    $view->assign('disallowedhtml', $disallowedhtml);
+
+                    // end fckeditor
+                    break;
+
+                case 'openwysiwyg':
+                    // get openwysiwyg config if editor is active
+                    // prepare areas for openwysiwyg
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } else {
+                        $modareas = $args['areas'];
+                    }
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+
+                    // end openwysiwyg
+                    break;
+
+                case 'nicedit':
+                    // get nicEditor config if editor is active
+                    // prepare areas for nicEditor
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } else {
+                        $modareas = $args['areas'];
+                    }
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+
+                    // end nicEditor
+                    break;
+
+                case 'yui':
+                    // set body class for YUI Editor
+                    PageUtil::SetVar('body', 'class="yui-skin-sam"');
+
+                    // get YUI mode from config
+                    $yui_type = ModUtil::getVar('Scribite', 'yui_type');
+
+                    // type switch
+                    if ($yui_type == 'Simple') {
+                        // load scripts for YUI simple mode
+                        PageUtil::AddVar('stylesheet', 'http://yui.yahooapis.com/2.6.0/build/assets/skins/sam/skin.css');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/element/element-beta-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/container/container_core-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/editor/simpleeditor-min.js');
+                    } else {
+                        // load scripts for YUI Editor full mode
+                        PageUtil::AddVar('stylesheet', 'http://yui.yahooapis.com/2.6.0/build/assets/skins/sam/skin.css');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/yahoo-dom-event/yahoo-dom-event.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/element/element-beta-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/container/container_core-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/menu/menu-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/button/button-min.js');
+                        PageUtil::AddVar('javascript', 'http://yui.yahooapis.com/2.6.0/build/editor/editor-min.js');
+                    }
+
+                    // prepare areas for YUI
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } else {
+                        $modareas = $args['areas'];
+                    }
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+
+                    // end yui
+                    break;
+
+                case 'ckeditor':
+                    // get CKEditor config if editor is active
+                    // prepare areas
+                    if ($args['areas'][0] == "all") {
+                        $modareas = 'all';
+                    } else {
+                        $modareas = $args['areas'];
+                    }
+
+                    // check for allowed html
+                    $AllowableHTML = System::getVar('AllowableHTML');
+                    $disallowedhtml = array();
+                    while (list($key, $access) = each($AllowableHTML)) {
+                        if ($access == 0) {
+                            $disallowedhtml[] = DataUtil::formatForDisplay($key);
+                        }
+                    }
+
+                    // load Prototype
+                    PageUtil::AddVar('javascript', 'javascript/ajax/prototype.js');
+
+                    // set parameters
+                    $view->assign('modareas', $modareas);
+                    $view->assign('disallowedhtml', $disallowedhtml);
+
+                    // end ckeditor
+                    break;
+            }
+
+            // view output
+            // 1. check if special template is required (from direct module call)
+            if (isset($args['tpl']) && $view->template_exists($args['tpl'])) {
+                $templatefile = $args['tpl'];
+                // 2. check if a module specific template exists
+            } elseif ($view->template_exists('scribite_' . $args['editor'] . '_' . $args['modulename'] . '.htm')) {
+                $templatefile = 'scribite_' . $args['editor'] . '_' . $args['modulename'] . '.htm';
+                // 3. if none of the above load default template
+            } else {
+                $templatefile = 'scribite_' . $args['editor'] . '_editorheader.htm';
+            }
+            $output = $view->fetch($templatefile);
+            // end main switch
+
+            return $output;
+        }
     }
 }
