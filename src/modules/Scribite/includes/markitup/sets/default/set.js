e38f0f8168314547f3f9688c30a0d999310e7ea8
dmm1 <workspace99@gmail.com>
2011-09-28 09:21:08 +0200

    update markitup to latest version, refs #12

--- src/modules/Scribite/includes/markitup/sets/default/set.js
+++ src/modules/Scribite/includes/markitup/sets/default/set.js
@@ -1,7 +1,7 @@
 // ----------------------------------------------------------------------------
 // markItUp!
 // ----------------------------------------------------------------------------
-// Copyright (C) 2008 Jay Salvat
+// Copyright (C) 2011 Jay Salvat
 // http://markitup.jaysalvat.com/
 // ----------------------------------------------------------------------------
 // Html tags
@@ -9,51 +9,22 @@
 // ----------------------------------------------------------------------------
 // Basic set. Feel free to add more tags
 // ----------------------------------------------------------------------------
-mySettings = {
-	onShiftEnter:	{keepDefault:false, replaceWith:'<br />\n'},
-	onCtrlEnter:	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>\n'},
-	onTab:			{keepDefault:false, openWith:'	 '},
-	markupSet: [
-		{name:'Heading 1', key:'1', openWith:'<h1(!( class="[![Class]!]")!)>', closeWith:'</h1>', placeHolder:'Your title here...' },
-		{name:'Heading 2', key:'2', openWith:'<h2(!( class="[![Class]!]")!)>', closeWith:'</h2>', placeHolder:'Your title here...' },
-		{name:'Heading 3', key:'3', openWith:'<h3(!( class="[![Class]!]")!)>', closeWith:'</h3>', placeHolder:'Your title here...' },
-		{name:'Heading 4', key:'4', openWith:'<h4(!( class="[![Class]!]")!)>', closeWith:'</h4>', placeHolder:'Your title here...' },
-		{name:'Heading 5', key:'5', openWith:'<h5(!( class="[![Class]!]")!)>', closeWith:'</h5>', placeHolder:'Your title here...' },
-		{name:'Heading 6', key:'6', openWith:'<h6(!( class="[![Class]!]")!)>', closeWith:'</h6>', placeHolder:'Your title here...' },
-		{name:'Paragraph', openWith:'<p(!( class="[![Class]!]")!)>', closeWith:'</p>' },
-		{separator:'---------------' },
+var mySettings = {
+	onShiftEnter:  	{keepDefault:false, replaceWith:'<br />\n'},
+	onCtrlEnter:  	{keepDefault:false, openWith:'\n<p>', closeWith:'</p>'},
+	onTab:    		{keepDefault:false, replaceWith:'    '},
+	markupSet:  [ 	
 		{name:'Bold', key:'B', openWith:'(!(<strong>|!|<b>)!)', closeWith:'(!(</strong>|!|</b>)!)' },
-		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)' },
+		{name:'Italic', key:'I', openWith:'(!(<em>|!|<i>)!)', closeWith:'(!(</em>|!|</i>)!)'  },
 		{name:'Stroke through', key:'S', openWith:'<del>', closeWith:'</del>' },
 		{separator:'---------------' },
 		{name:'Bulleted List', openWith:'    <li>', closeWith:'</li>', multiline:true, openBlockWith:'<ul>\n', closeBlockWith:'\n</ul>'},
 		{name:'Numeric List', openWith:'    <li>', closeWith:'</li>', multiline:true, openBlockWith:'<ol>\n', closeBlockWith:'\n</ol>'},
-		{name:'Li', openWith:'<li>', closeWith:'</li>' },
 		{separator:'---------------' },
-		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
 		{name:'Picture', key:'P', replaceWith:'<img src="[![Source:!:http://]!]" alt="[![Alternative text]!]" />' },
+		{name:'Link', key:'L', openWith:'<a href="[![Link:!:http://]!]"(!( title="[![Title]!]")!)>', closeWith:'</a>', placeHolder:'Your text to link...' },
 		{separator:'---------------' },
-		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },
-		{name:'TinyUrl', className:'tinyUrl', openWith:function(markItUp) { return miu.tinyUrl(markItUp) }, closeWith:'</a>', placeHolder:'text to link with a long url...' },
-		{separator:'---------------' },
-		{name:'Preview', className:'preview', call:'preview' }
-		
+		{name:'Clean', className:'clean', replaceWith:function(markitup) { return markitup.selection.replace(/<(.*?)>/g, "") } },		
+		{name:'Preview', className:'preview',  call:'preview'}
 	]
-}
-// mIu nameSpace to avoid conflict.
-miu = {      
-   tinyUrl: function (markItUp) {
-        var tinyUrl = '',
-        	url = prompt("Url:", "http://");
-		if (url == null) {
-			return false;
-		}
-        
-        jQuery.ajaxSetup( { async:false, global:false } );
-        jQuery.post("modules/Scribite/includes/markitup/sets/default/utils/tinyurl/get.php", "url="+url, function(content) {
-        	tinyUrl = content;    
-        });
-
-        return '<a href="'+tinyUrl+'"(!( title="[![Title]!]")!)>';
-    }
 }
