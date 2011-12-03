bb35beb00a8174ec8fe0a87d3a159073aaa70812
Drak <drak@zikula.org>
2011-12-03 09:16:44 +0545

    Set core requirement to 1.3.x

--- src/modules/Scribite/lib/Scribite/Version.php
+++ src/modules/Scribite/lib/Scribite/Version.php
@@ -17,7 +17,8 @@ class Scribite_Version extends Zikula_AbstractVersion
         $meta['oldnames'] = array('scribite');
         $meta['url'] = $this->__('scribite');
         $meta['version'] = '4.3.0';
-        $meta['core_min'] = '1.3.0';
+        $meta['core_min'] = '1.3.0'; // Fixed to 1.3.x range
+        $meta['core_max'] = '1.3.99'; // Fixed to 1.3.x range
         $meta['description'] = $this->__('WYSIWYG for Zikula');
         $meta['securityschema'] = array('Scribite::' => 'Modulename::');
 // openwysiwyg deprecated @4.3.0
