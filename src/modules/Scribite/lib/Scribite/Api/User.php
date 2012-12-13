a19c3b8e7e74802263acfb607eb0db55898d5606
Craig Heydenburg <craigh@mac.com>
2012-12-12 21:19:04 -0500

    relocate (to complete the separation) a xinha-specific api method. Also attempt to enable it's use (non tested and still disabled  use of EFM)

--- src/modules/Scribite/lib/Scribite/Api/User.php
+++ src/modules/Scribite/lib/Scribite/Api/User.php
@@ -81,47 +81,6 @@ class Scribite_Api_User extends Zikula_AbstractApi
         return $instance->getMetaDisplayName();
     }
 
-    // load IM/EFM settings for Xinha and pass vars to session
-    // not implemented yet ;)
-    public function getEFMConfig($args)
-    {
-        // get editors path and load xinha scripts
-        $editors_path = $this->getVar('editors_path');
-        require_once $editors_path . '/xinha/contrib/php-xinha.php';
-
-        $zikulaBaseURI = rtrim(System::getBaseUri(), '/');
-        $zikulaBaseURI = ltrim($zikulaBaseURI, '/');
-        $zikulaRoot = rtrim($_SERVER['DOCUMENT_ROOT'], '/');
-
-        // define backend configuration for the plugin
-        $IMConfig = array();
-        $IMConfig['images_dir'] = '/files/';
-        $IMConfig['images_url'] = 'files/';
-        $IMConfig['files_dir'] = '/files/';
-        $IMConfig['files_url'] = 'files';
-        $IMConfig['thumbnail_prefix'] = 't_';
-        $IMConfig['thumbnail_dir'] = 't';
-        $IMConfig['resized_prefix'] = 'resized_';
-        $IMConfig['resized_dir'] = '';
-        $IMConfig['tmp_prefix'] = '_tmp';
-        $IMConfig['max_filesize_kb_image'] = 2000;
-        // maximum size for uploading files in 'insert image' mode (2000 kB here)
-
-        $IMConfig['max_filesize_kb_link'] = 5000;
-        // maximum size for uploading files in 'insert link' mode (5000 kB here)
-        // Maximum upload folder size in Megabytes.
-        // Use 0 to disable limit
-        $IMConfig['max_foldersize_mb'] = 0;
-
-        $IMConfig['allowed_image_extensions'] = array("jpg", "gif", "png");
-        $IMConfig['allowed_link_extensions'] = array("jpg", "gif", "pdf", "ip", "txt",
-                "psd", "png", "html", "swf",
-                "xml", "xls");
-
-        xinha_pass_to_php_backend($IMConfig);
-        return $IMConfig;
-    }
-
     /**
      * Initialise Scribite for requested areas.
      *
