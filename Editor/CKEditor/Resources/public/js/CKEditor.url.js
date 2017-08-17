bf12794cbc8cdc6b88fa8a8133b31cf005677305
Craig Heydenburg <craigh@mac.com>
2017-08-17 07:41:45 -0400

    correct url path

--- Editor/CKEditor/Resources/public/js/CKEditor.url.js
+++ Editor/CKEditor/Resources/public/js/CKEditor.url.js
@@ -1,2 +1,2 @@
 // Make sure to work with jcss combination enabled.
-var CKEDITOR_BASEPATH = Zikula.Config.baseURL + Zikula.Config.baseURI + '/modules/Scribite/plugins/CKEditor/vendor/ckeditor/';
+var CKEDITOR_BASEPATH = Zikula.Config.baseURL + Zikula.Config.baseURI + '/web/editors/ckeditor/ckeditor/';
