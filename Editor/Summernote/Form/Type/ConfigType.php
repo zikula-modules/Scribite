9876cef1d60d72ba69ab3a9bb8d2c5b37acf94d0
Guite <info@guite.de>
2017-09-04 13:27:40 +0200

    ci

--- Editor/Summernote/Form/Type/ConfigType.php
+++ Editor/Summernote/Form/Type/ConfigType.php
@@ -11,7 +11,6 @@
 
 namespace Zikula\ScribiteModule\Editor\Summernote\Form\Type;
 
-use Symfony\Component\Finder\Finder;
 use Symfony\Component\Form\AbstractType;
 use Symfony\Component\Form\Extension\Core\Type\CheckboxType;
 use Symfony\Component\Form\Extension\Core\Type\IntegerType;
